3a7b4d1e044f4917e8f17c485bda8c6ca7068eb1
Ayman Nageeb <aimangta123121@gmail.com>
2022-12-08 00:52:29 +0200

    Feature: set cors with authorization-token

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,7 +16,7 @@ class CorsMiddleware
       'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
       'Access-Control-Allow-Credentials' => 'true',
       'Access-Control-Max-Age'           => '86400',
-      'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With, api-key, is-host, authorization-token'
+      'Access-Control-Allow-Headers'     => 'Content-Type, X-Requested-With, Authorization-Token'
     ];
 
     if ($request->isMethod('OPTIONS')) {
